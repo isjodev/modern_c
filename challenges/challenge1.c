1ec31c1bcea54e38cc389132dae090810917aa3a
Jackson Smith <i-s-j-o@proton.me>
2024-05-01 10:44:47 -0400

    added Makefile, completed quick_sort for integers

--- challenges/challenge1.c
+++ challenges/challenge1.c
@@ -16,21 +16,26 @@
 // quick_sort.h
 
 // Number of elements in array.
-#define LEN 10
+#define LEN 5
 
 // Function prototypes.
 int *init_int_array(int len);
 void fill_int_array(int *array, int len);
 void print_int_array(int *array, int len);
 void swap(int *, int *);
-void partition(void);
-void quick_sort(void);
+int partition(int *array, int start, int end);
+void quick_sort(int *array, int start, int end);
 void merge_sort(void);
 
 int main(void) {
   int *array = init_int_array(LEN);
   fill_int_array(array, LEN);
-  // int n = sizeof(array) / sizeof(array[0]);
+
+  // Print
+  print_int_array(array, LEN);
+
+  // Sort
+  quick_sort(array, 0, LEN - 1);
 
   // Print
   print_int_array(array, LEN);
@@ -47,7 +52,7 @@ void fill_int_array(int *array, int len) {
   for (size_t i = 0; i < len; i++) {
     // Random integer value between 0 - 100
     int rand_val = rand() % 100;
-    *(array + i) = rand_val;
+    array[i] = rand_val;
   }
 }
 
@@ -55,6 +60,7 @@ void print_int_array(int *array, int len) {
   for (size_t i = 0; i < len; i++) {
     printf("%d ", *(array + i));
   }
+  printf("\n");
 }
 
 void swap(int *a, int *b) {
@@ -64,22 +70,23 @@ void swap(int *a, int *b) {
 }
 
 // Input: Array, start, end
-void partition(void) {
-  // pivot = array[end]
-  // pivot_idx = start
-  // loop idx from start to end
-  //   if array[index] <= pivot_value
-  //     swap(array[index], array[pivot_idx])
-  //     pivot_idx++
-  // return pivot_idx - 1
+int partition(int *array, int start, int end) {
+  int pivot = array[end];
+  int pivot_idx = (start - 1);
+  for (int i = start; i < end; i++) {
+    if (array[i] <= pivot) {
+      pivot_idx++;
+      swap(&array[i], &array[pivot_idx]);
+    }
+  }
+  swap(&array[pivot_idx + 1], &array[end]);
+  return (pivot_idx + 1);
 }
 
-void quick_sort(void) {
-  // if start >= end
-  //   return
-  // pivot_idx = partition(array, start, end)
-  // quick_sort(array, start, pivot_idx - 1)
-  // quick_sort(aray, pivot_idx + 1, end)
+void quick_sort(int *array, int start, int end) {
+  if (start < end) {
+    int pivot_idx = partition(array, start, end);
+    quick_sort(array, start, pivot_idx - 1);
+    quick_sort(array, pivot_idx + 1, end);
+  }
 }
-
-void merge_sort(void) {}
