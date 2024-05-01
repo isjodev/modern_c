0defad43ae2549a8e662c028d453d36cc7577ffe
Jackson Smith <i-s-j-o@proton.me>
2024-05-01 13:00:29 -0400

    completed quick_sort implementation, added TODO

--- challenges/challenge1.c
+++ challenges/challenge1.c
@@ -4,6 +4,10 @@
  * on arrays with sort keys such as double or strings
  * to your liking.  */
 
+/* TODO: Implement quick_sort for both strings and doubles.
+ * TODO: Provide error handing.
+ * TODO: Implement merge_sort().  */
+
 // Preprocessor.
 #include <stdbool.h>
 #include <stdint.h>
@@ -25,30 +29,37 @@ void print_int_array(int *array, int len);
 void swap(int *, int *);
 int partition(int *array, int start, int end);
 void quick_sort(int *array, int start, int end);
-void merge_sort(void);
 
 int main(void) {
+  // Initialize and fill integer array with LEN values.
   int *array = init_int_array(LEN);
   fill_int_array(array, LEN);
 
-  // Print
+  // Print Array
   print_int_array(array, LEN);
 
-  // Sort
+  // Sort Array
   quick_sort(array, 0, LEN - 1);
 
-  // Print
+  // Print Array
   print_int_array(array, LEN);
   return 0;
 }
 
+/* init_int_array takes takes intger value len and dynamically allocates
+ * an array of length len, and returns a pointer to array. */
 int *init_int_array(int len) {
+  // Dynamically allocate array.
   int *int_array = malloc(sizeof(int) * len);
   return int_array;
 }
 
+/* fill_int_array randomly generates integers and populates array. */
 void fill_int_array(int *array, int len) {
+  // Seed random.
   srand(time(NULL));
+
+  // Loop over array.
   for (size_t i = 0; i < len; i++) {
     // Random integer value between 0 - 100
     int rand_val = rand() % 100;
@@ -56,6 +67,8 @@ void fill_int_array(int *array, int len) {
   }
 }
 
+/* print_int_array loops over the length of an array and prints the
+ * value at each address. */
 void print_int_array(int *array, int len) {
   for (size_t i = 0; i < len; i++) {
     printf("%d ", *(array + i));
@@ -63,27 +76,45 @@ void print_int_array(int *array, int len) {
   printf("\n");
 }
 
+// Swap two pointers.
 void swap(int *a, int *b) {
   int temp = *a;
   *a = *b;
   *b = temp;
 }
 
-// Input: Array, start, end
+/* partition takes an array, start, and end index,
+ * swaps any values which are less than the pivot value,
+ * then returns the array index after the pivot value. */
 int partition(int *array, int start, int end) {
+  // Use last element in arry as pivot value.
   int pivot = array[end];
+
+  // Subarray begins with element behind first value.
   int pivot_idx = (start - 1);
+
+  // Loop through array, swapping any values which are less than or equal
+  // to the pivot value.
   for (int i = start; i < end; i++) {
     if (array[i] <= pivot) {
+      // Increment pivot index.
       pivot_idx++;
       swap(&array[i], &array[pivot_idx]);
     }
   }
+
+  // Swap final value with pivot value.
   swap(&array[pivot_idx + 1], &array[end]);
+
+  // Return index of final swap position.
   return (pivot_idx + 1);
 }
 
+/* quick_sort takes an array, a starting index, and a final index.
+ * If the starting index is less than the final index,
+ * partition the array into subarrays and sort each subarray. */
 void quick_sort(int *array, int start, int end) {
+  // TODO: Error handling.
   if (start < end) {
     int pivot_idx = partition(array, start, end);
     quick_sort(array, start, pivot_idx - 1);
